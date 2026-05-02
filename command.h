5e3f4946c3fef22ba03dc3a49026544f0dde1384
anton-tchekov <anton@tchekov.net>
2026-05-02 16:22:04 +0000

    Smooth movement

--- command.h
+++ command.h
@@ -40,6 +40,35 @@ void process_token(char *buf)
 		}
 	}
 
+	if(!strcmp(buf, "zero"))
+	{
+		set_zero();
+		Serial.println("Set current position as zero");
+		return;
+	}
+
+	if(!strcmp(buf, "move"))
+	{
+		long radius = 0;
+		if(!stack_pop(&radius))
+		{
+			err_underflow();
+			return;
+		}
+
+		long angle_deg = 0;
+		if(!stack_pop(&angle_deg))
+		{
+			err_underflow();
+			return;
+		}
+
+		Serial.printf("Moving to %d deg, %d mm\n", angle_deg, radius);
+
+		move_to(angle_deg / 180.0 * M_PI, radius);
+		return;
+	}
+
 	if(!strcmp(buf, "delay"))
 	{
 		long val = 0;
--- movement.h
+++ movement.h
@@ -3,7 +3,9 @@ static int cur_pos_y = 0;
 static int y_steps_since_extra = 0;
 static bool cur_x_dir = false;
 static bool cur_y_dir = false;
-static int step_delay_us = 800;
+static int step_delay_us = 200;
+
+#define MAX_Y_STEPS 16000
 
 // X and Y in mm (-300 to +300)
 // A (angle) for Y motor pos
@@ -62,11 +64,21 @@ void step_y(bool dir)
 	if(dir)
 	{
 		cur_pos_y++;
+		if(cur_pos_y >= MAX_Y_STEPS)
+		{
+			cur_pos_y = 0;
+		}
+
 		y_steps_since_extra++;
 	}
 	else
 	{
 		cur_pos_y--;
+		if(cur_pos_y < 0)
+		{
+			cur_pos_y = MAX_Y_STEPS - 1;
+		}
+
 		y_steps_since_extra--;
 	}
 
@@ -79,5 +91,70 @@ void step_y(bool dir)
 	{
 		step_x_untracked(false);
 		y_steps_since_extra = 0;
+	}
+}
+
+void set_zero(void)
+{
+	cur_pos_x = 0;
+	cur_pos_y = 0;
+}
+
+void move_to(double angle, double radius)
+{
+	angle = fmod(angle, 2.0 * M_PI);
+	if(angle < 0.0)
+	{
+		angle += 2.0 * M_PI;
+	}
+
+	int target_x = motor_x_steps(radius);
+	int target_y = motor_y_steps(angle);
+
+	int diff_x = target_x - cur_pos_x;
+	int diff_y = target_y - cur_pos_y;
+
+	if(diff_y > MAX_Y_STEPS / 2)
+	{
+		diff_y = MAX_Y_STEPS - diff_y;
+	}
+	else if(diff_y < -MAX_Y_STEPS / 2)
+	{
+		diff_y = MAX_Y_STEPS + diff_y;
+	}
+
+	int steps_x = abs(diff_x);
+	int steps_y = abs(diff_y);
+
+	int max_steps = max(steps_x, steps_y);
+	bool x_main = (steps_x == max_steps);
+	double ratio = x_main ? (steps_y / (double)steps_x) : (steps_x / (double)steps_y);
+
+	int other_count = 0;
+	for(int i = 0; i < max_steps; ++i)
+	{
+		if(x_main)
+		{
+			step_x(diff_x > 0);
+		}
+		else
+		{
+			step_y(diff_y > 0);
+		}
+
+		double error = ratio * i - other_count;
+		if(error >= 1.0)
+		{
+			if(x_main)
+			{
+				step_y(diff_y > 0);
+			}
+			else
+			{
+				step_x(diff_x > 0);
+			}
+
+			other_count ++;
+		}
 	}
 }
